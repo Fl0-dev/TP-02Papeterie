c4e093ffb85e8b4ff871676e86d8049b1fcf758c
FL0-dev <florian.girard.dev@outlook.fr>
2021-06-04 16:47:12 +0200

    logs

--- src/fr/eni/papeterie/ihm/GUI.java
+++ src/fr/eni/papeterie/ihm/GUI.java
@@ -236,7 +236,7 @@ public class GUI extends JFrame {
     //Singleton Button
     public JButton getAvant() {
         if (avant == null) {
-            Icon icon = new ImageIcon("src/fr/eni/papeterie/ihm/resources/Forward24.gif");
+            Icon icon = new ImageIcon("resources/Forward24.gif");
             avant = new JButton(icon);
             avant.addActionListener(new ActionListener() {
                 @Override
@@ -301,7 +301,7 @@ public class GUI extends JFrame {
 
     public JButton getArriere() {
         if (arriere == null) {
-            Icon icon = new ImageIcon("src/fr/eni/papeterie/ihm/resources/Back24.gif");
+            Icon icon = new ImageIcon("resources/Back24.gif");
             arriere = new JButton(icon);
             arriere.addActionListener(new ActionListener() {
                 @Override
@@ -366,7 +366,7 @@ public class GUI extends JFrame {
 
     public JButton getRemove() {
         if (remove == null) {
-            Icon icon = new ImageIcon("src/fr/eni/papeterie/ihm/resources/Delete24.gif");
+            Icon icon = new ImageIcon("resources/Delete24.gif");
             remove = new JButton(icon);
             //action du bouton
             remove.addActionListener(new ActionListener() {
@@ -395,7 +395,7 @@ public class GUI extends JFrame {
     public JButton getSave() {
         //création du bouton
         if (save == null) {
-            Icon icon = new ImageIcon("src/fr/eni/papeterie/ihm/resources/Save24.gif");
+            Icon icon = new ImageIcon("resources/Save24.gif");
             save = new JButton(icon);
             //Action du bouton save
             save.addActionListener(new ActionListener() {
@@ -472,7 +472,7 @@ public class GUI extends JFrame {
 
     public JButton getNvl() {
         if (nvl == null) {
-            Icon icon = new ImageIcon("src/fr/eni/papeterie/ihm/resources/New24.gif");
+            Icon icon = new ImageIcon("resources/New24.gif");
             nvl = new JButton(icon);
             //action nvl
             nvl.addActionListener(new ActionListener() {
--- src/fr/eni/papeterie/ihm/Main.java
+++ src/fr/eni/papeterie/ihm/Main.java
@@ -1,21 +1,35 @@
 package fr.eni.papeterie.ihm;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.swing.*;
 
 /**
  * Permet de lancer l'affichage créé
  */
 public class Main {
+
+    //création d'un logger en attribut de classe
+    private static Logger logMain = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
+        //placement de log
+        logMain.debug("Démarrage du programme");
         //création d'un écran principal
         SwingUtilities.invokeLater(
                 //on pourrait faire une lambda (peu importe le nom lance)
                 new Runnable() {
+
                     @Override
                     public void run() {
+                        //placement de log
+                        logMain.debug("Lancement de GUI");
                         JFrame app =new GUI();
                     }
                 }
         );
+        //placement de log
+        logMain.debug("Fin du programme");
     }
 }
